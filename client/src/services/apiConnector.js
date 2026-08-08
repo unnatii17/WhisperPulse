0e1efaf48b12a7eac7cebe18085cee1789082bf5
Unnati <unnati7200@gmail.com>
2026-08-08 06:45:28 +0530

    Update apiConnector.js

--- client/src/services/apiConnector.js
+++ client/src/services/apiConnector.js
@@ -1,44 +1,41 @@
-import axios from 'axios';
-import { setUser } from '../slices/profileSlice';
-import { setToken } from '../slices/authSlice';
-import { store } from '../index';
-import {toast} from "react-hot-toast";
+import axios from "axios";
+import { setUser } from "../slices/profileSlice";
+import { setToken } from "../slices/authSlice";
+import { store } from "../index";
+import { toast } from "react-hot-toast";
+
 export const axiosInstance = axios.create({
-  baseURL:"http://localhost:9001/api/v1",  
-  withCredentials: true, 
+  withCredentials: true,
 });
 
-
 axiosInstance.interceptors.response.use(
-  response => response,
-  error => {
+  (response) => response,
+  (error) => {
     if (error.response && error.response.status === 401) {
-      let toastId;
-      setTimeout(()=>{toastId=toast.loading("Session expired,logging out...")},1*500)
-      setTimeout(()=>{
-        localStorage.removeItem('token');
-        localStorage.removeItem('user');
-     
+      const toastId = toast.loading("Session expired, logging out...");
+
+      setTimeout(() => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+
         store.dispatch(setUser(null));
         store.dispatch(setToken(null));
-        toast.dismiss(toastId)  
-      },1*1000)
 
-      
-      
-      setTimeout(()=>{toast.error("Session Expired,Login again")},2*1000)
-      
+        toast.dismiss(toastId);
+        toast.error("Session Expired, Login again");
+      }, 1000);
     }
+
     return Promise.reject(error);
   }
 );
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
   return axiosInstance({
-    method: `${method}`,
-    url: `${url}`,
-    data: bodyData ? bodyData : null,
-    headers: headers ? headers : {},
-    params: params ? params : {},
+    method,
+    url,
+    data: bodyData || null,
+    headers: headers || {},
+    params: params || {},
   });
 };
