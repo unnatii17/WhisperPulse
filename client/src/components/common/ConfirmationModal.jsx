3e33ba49a8b12486277ea6e014f1b64901a1db7d
Unnati <unnati7200@gmail.com>
2026-08-08 05:49:00 +0530

    Update ConfirmationModal.jsx

--- client/src/components/common/ConfirmationModal.jsx
+++ client/src/components/common/ConfirmationModal.jsx
@@ -1,40 +1,36 @@
-import { memo, useRef, useState } from "react"
-import SubmitButton from "../common/SubmitButton"
+import { memo, useRef } from "react";
+import SubmitButton from "../common/SubmitButton";
 import useOnClickOutside from "../../hooks/useOnClickOutside";
-import useEscape from '../../hooks/useEscape';
-
+import useEscape from "../../hooks/useEscape";
 
 export default memo(function ConfirmationModal({ modalData, setModal }) {
-  // const [isVisible, setIsVisible] = useState(true);
   const modalRef = useRef();
 
   const modalHandler = () => {
-    // setIsVisible(false);
-    setTimeout(() => setModal(false), 300); // Delay to allow exit animation to complete
+    setTimeout(() => setModal(false), 300);
   };
 
   useOnClickOutside(modalRef, modalHandler);
   useEscape(modalRef, modalHandler);
+
   return (
-    <div ref={modalRef} className="fixed inset-0 z-[1000] !mt-0 grid place-items-center overflow-auto shadow-lg bg-black bg-opacity-10 backdrop-blur-sm">
-      <div className="w-11/12 max-w-[350px] rounded-lg bg-[#fffcec] dark:bg-confettiDarkColor3 border border-black dark:border-yellow-200  p-6">
-        <p className="text-2xl font-semibold text-richblack-5">
-          {modalData?.text1}
-        </p>
-        <p className="mt-3 mb-5 leading-6 ">
-          {modalData?.text2}
-        </p>
-        <div className="flex mx-5 justify-between gap-x-4">
+    <div>
+      <div ref={modalRef}>
+        <h2>{modalData?.text1}</h2>
+
+        <p>{modalData?.text2}</p>
+
+        <div>
           <SubmitButton
-          customClasses="text-black rounded-md font-semibold "
+            customClasses="text-black rounded-md font-semibold"
             type="submit"
             onclick={modalData?.btn1Handler}
             disabled={false}
             text={modalData?.btn1Text}
-
           />
+
           <button
-            className="cursor-pointer rounded-md  py-[8px] px-[20px] font-semibold "
+            className="cursor-pointer rounded-md py-[8px] px-[20px] font-semibold"
             onClick={modalData?.btn2Handler}
           >
             {modalData?.btn2Text}
@@ -42,5 +38,5 @@ export default memo(function ConfirmationModal({ modalData, setModal }) {
         </div>
       </div>
     </div>
-  )
-})
+  );
+});
