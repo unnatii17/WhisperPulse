7ba961505cc63eb752f89ff8fc669e72ddf77a95
Unnati <unnati7200@gmail.com>
2026-08-08 14:38:38 +0530

    Update mailSender.js

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,31 +1,35 @@
-const nodemailer=require('nodemailer');
+const nodemailer = require("nodemailer");
 require("dotenv").config();
-const mailSender=async(email,title,body)=>{
-  try{
-    //Create Transporter
-    let transporter=nodemailer.createTransport({
-      host:process.env.MAIL_HOST,
-      port: process.env.MAIL_PORT,
-      auth:{
-        user:process.env.MAIL_USER,
-        pass:process.env.MAIL_PASS,
+
+const mailSender = async (email, title, body) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      port: Number(process.env.MAIL_PORT),
+      secure: false,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
       },
     });
 
-    //Send mail
-    let info=await transporter.sendMail({
-      from:"WhisperPulse",
-      to:`${email}`,
-      subject:`${title}`,
-      html:`${body}`
+    await transporter.verify();
+    console.log("SMTP Connected Successfully");
+
+    const info = await transporter.sendMail({
+      from: `"WhisperPulse" <${process.env.MAIL_USER}>`,
+      to: email,
+      subject: title,
+      html: body,
     });
 
+    console.log("Mail Sent:", info.messageId);
+
     return info;
+  } catch (err) {
+    console.error("MAIL ERROR:", err);
+    throw err;
   }
-  catch(err){
-    console.log(err.message);
-  }
-}
+};
 
-
-module.exports=mailSender;
+module.exports = mailSender;
