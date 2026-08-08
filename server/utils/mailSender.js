b8c0da33bb5cdb60a45a9cd0d83e60c984fe6522
Unnati <unnati7200@gmail.com>
2026-08-08 17:46:25 +0530

    Update mailSender.js

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -2,20 +2,11 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: 587,
-  secure: false,
-  requireTLS: true,
+  service: "gmail",
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
-  tls: {
-    rejectUnauthorized: false,
-  },
-  connectionTimeout: 60000,
-  greetingTimeout: 60000,
-  socketTimeout: 60000,
 });
 
 const mailSender = async (email, title, body) => {
@@ -24,13 +15,13 @@ const mailSender = async (email, title, body) => {
     console.log("SMTP Connected Successfully");
 
     const info = await transporter.sendMail({
-      from: `"WhisperPulse" <${process.env.MAIL_USER}>`,
+      from: process.env.MAIL_USER,
       to: email,
       subject: title,
       html: body,
     });
 
-    console.log("Mail Sent:", info.messageId);
+    console.log("Mail Sent Successfully:", info.messageId);
     return info;
   } catch (err) {
     console.error("MAIL ERROR:", err);
