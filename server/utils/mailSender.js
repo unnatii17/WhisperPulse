c4e7abf073908fe2132f6aa55093beb2777483a0
Unnati <unnati7200@gmail.com>
2026-08-08 17:17:04 +0530

    Update mailSender.js

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,18 +1,25 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
+const transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  connectionTimeout: 60000,
+  greetingTimeout: 60000,
+  socketTimeout: 60000,
+});
+
 const mailSender = async (email, title, body) => {
   try {
-    const transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
-      port: Number(process.env.MAIL_PORT),
-      secure: false,
-      auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
-      },
-    });
-
     await transporter.verify();
     console.log("SMTP Connected Successfully");
 
@@ -24,7 +31,6 @@ const mailSender = async (email, title, body) => {
     });
 
     console.log("Mail Sent:", info.messageId);
-
     return info;
   } catch (err) {
     console.error("MAIL ERROR:", err);
