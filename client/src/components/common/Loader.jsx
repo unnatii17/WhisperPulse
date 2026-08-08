2a34c162c317f7e773cdc6008b0a4e1245b6b511
Unnati <unnati7200@gmail.com>
2026-08-09 04:53:18 +0530

    Update Loader.jsx

--- client/src/components/common/Loader.jsx
+++ client/src/components/common/Loader.jsx
@@ -1,21 +1,18 @@
-import React from 'react'
-import { Hearts } from 'react-loader-spinner'
-
+import React from "react";
+import { Hearts } from "react-loader-spinner";
 
 const Loader = () => {
   return (
-    <div className='w-full h-full grid place-items-center'>
-        <Hearts
-            height="80"
-            width="80"
-            color="#DC143C"
-            ariaLabel="hearts-loading"
-            wrapperStyle={{}}
-            wrapperClass=""
-            visible={true}
-        />
+    <div className="min-h-screen w-full bg-gradient-to-br from-gray-950 via-gray-900 to-black grid place-items-center">
+      <Hearts
+        height="80"
+        width="80"
+        color="#DC143C"
+        ariaLabel="hearts-loading"
+        visible={true}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Loader
+export default Loader;
