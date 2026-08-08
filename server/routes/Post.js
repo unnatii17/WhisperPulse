7d60af1dddc479804b6524c4c475db019ac3ac9c
Unnati <unnati7200@gmail.com>
2026-08-09 05:24:45 +0530

    Update Post.js

--- server/routes/Post.js
+++ server/routes/Post.js
@@ -1,16 +1,66 @@
-const { auth, isAdmin } = require("../middlewares/auth")
-const { createPost, editPost, deletePost, getPosts, getUserPosts, reportPost, getUserPostsStats, postExist } = require("../controllers/Post")
-const { Router } = require("express")
+const {
+  auth,
+  isAdmin,
+} = require("../middlewares/auth");
+
+const {
+  createPost,
+  editPost,
+  deletePost,
+  getPosts,
+  getUserPosts,
+  reportPost,
+  getUserPostsStats,
+  postExist,
+} = require("../controllers/Post");
+
+const { Router } = require("express");
 
 const router = Router();
 
-router.route("/create-post").post(auth, createPost);
-router.route("/edit-post").post(auth, editPost);
-router.route("/delete-post").post(auth, deletePost);
-router.route("/get-post").get(auth, getPosts);
-router.route("/get-user-posts").get(auth, getUserPosts);
-router.route("/report-post").post(auth, reportPost);
-router.route("/get-user-stats").get(auth,getUserPostsStats);
-router.route("/post-exists").get(auth,getUserPostsStats);
-router.route("/post-exist").post(auth,postExist);
+// Create Post
+router
+  .route("/create-post")
+  .post(auth, createPost);
+
+// Edit Post
+router
+  .route("/edit-post")
+  .post(auth, editPost);
+
+// Delete Post
+router
+  .route("/delete-post")
+  .post(auth, deletePost);
+
+// Get All Posts
+router
+  .route("/get-post")
+  .get(auth, getPosts);
+
+// Get User Posts
+router
+  .route("/get-user-posts")
+  .get(auth, getUserPosts);
+
+// Report Post
+router
+  .route("/report-post")
+  .post(auth, reportPost);
+
+// Get User Post Statistics
+router
+  .route("/get-user-stats")
+  .get(auth, getUserPostsStats);
+
+// Check Post Exists
+router
+  .route("/post-exists")
+  .get(auth, postExist);
+
+// Check Post Exists - POST
+router
+  .route("/post-exist")
+  .post(auth, postExist);
+
 module.exports = router;
